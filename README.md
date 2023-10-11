72238477ff42ac134042fdd27bb6594976b4366d
Blitz <93739827+blitzexlmao@users.noreply.github.com>
2023-10-11 13:14:55 -0400

    some fixes + others

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SoniFlixel
-Basically a rewrite of ProjectCD but aimed to make the Sonic physics more accurate
+
+* Basically a rewrite of ProjectCD but aimed to make the Sonic physics more accurate
+
+# Creds
+
+* BlitzDotCS - Main Dev
+
+* GallantGlobz - Tweaked Sonic Sprites
--- source/Paths.hx
+++ source/Paths.hx
@@ -36,11 +36,6 @@ class Paths
 		return getPreloadPath(file);
 	}
 
-	inline static public function getHaxeScript(key:String)
-	{
-		return OpenFlAssets.getText('assets/data/songs/$key.hx');
-	}
-
 	/**
 	 * For a given key and library for an image, returns the corresponding BitmapData.
 	 		* We can probably move the cache handling here.
@@ -96,16 +91,6 @@ class Paths
 		return getPath(file, type, library);
 	}
 
-	inline static public function lua(key:String, ?library:String)
-	{
-		return getPath('data/$key.lua', TEXT, library);
-	}
-
-	inline static public function luaImage(key:String, ?library:String)
-	{
-		return getPath('data/$key.png', IMAGE, library);
-	}
-
 	inline static public function txt(key:String, ?library:String)
 	{
 		return getPath('$key.txt', TEXT, library);
@@ -116,9 +101,9 @@ class Paths
 		return getPath('data/$key.xml', TEXT, library);
 	}
 
-	inline static public function json(key:String, ?library:String)
+	inline static public function levels(key:String, ?library:String)
 	{
-		return getPath('data/songs/$key.json', TEXT, library);
+		return getPath('levels/$key.json', TEXT, library);
 	}
 
 	static public function sound(key:String, ?library:String)
@@ -136,38 +121,6 @@ class Paths
 		return getPath('music/$key.$SOUND_EXT', MUSIC, library);
 	}
 
-	inline static public function voices(song:String)
-	{
-		var songLowercase = StringTools.replace(song, " ", "-").toLowerCase();
-		switch (songLowercase)
-		{
-			case 'dad-battle':
-				songLowercase = 'dadbattle';
-			case 'philly-nice':
-				songLowercase = 'philly';
-			case 'm.i.l.f':
-				songLowercase = 'milf';
-		}
-		var result = 'songs:assets/songs/${songLowercase}/Voices.$SOUND_EXT';
-		// Return null if the file does not exist.
-		return doesSoundAssetExist(result) ? result : null;
-	}
-
-	inline static public function inst(song:String)
-	{
-		var songLowercase = StringTools.replace(song, " ", "-").toLowerCase();
-		switch (songLowercase)
-		{
-			case 'dad-battle':
-				songLowercase = 'dadbattle';
-			case 'philly-nice':
-				songLowercase = 'philly';
-			case 'm.i.l.f':
-				songLowercase = 'milf';
-		}
-		return 'songs:assets/songs/${songLowercase}/Inst.$SOUND_EXT';
-	}
-
 	inline static public function video(key:String)
 	{
 		return 'assets/videos/$key';
@@ -176,48 +129,6 @@ class Paths
 	inline static public function font(key:String)
 	{
 		return 'assets/fonts/$key';
-	}
-
-	inline static public function getAnimateAtlas(key:String)
-	{
-		return animate.FlxAnimate.fromAnimate(loadImage('$key/spritemap1'), file('images/$key/spritemap1.json'));
-	}
-
-	static public function listSongsToCache()
-	{
-		// We need to query OpenFlAssets, not the file system, because of Polymod.
-		var soundAssets = OpenFlAssets.list(AssetType.MUSIC).concat(OpenFlAssets.list(AssetType.SOUND));
-
-		// TODO: Maybe rework this to pull from a text file rather than scan the list of assets.
-		var songNames = [];
-
-		for (sound in soundAssets)
-		{
-			var path = sound.split('/');
-			// assets/songs/name/file.ext
-			if (path.length < 4)
-				continue;
-
-			if (path[1] != 'songs')
-				continue;
-
-			var songName = path[2];
-
-			// Remove duplicates.
-			if (songNames.indexOf(songName) != -1)
-				continue;
-
-			songNames.push(songName);
-		}
-
-		return songNames;
-	}
-
-	static public function doesSoundAssetExist(path:String)
-	{
-		if (path == null || path == "")
-			return false;
-		return OpenFlAssets.exists(path, AssetType.SOUND) || OpenFlAssets.exists(path, AssetType.MUSIC);
 	}
 
 	inline static public function doesTextAssetExist(path:String)
@@ -238,16 +149,4 @@ class Paths
 		}
 		return FlxAtlasFrames.fromSparrow(loadImage(key, library), file('images/$key.xml', library));
 	}
-
-	/**
-	 * Senpai in Thorns uses this instead of Sparrow and IDK why.
-	 */
-	inline static public function getPackerAtlas(key:String, ?library:String, ?isCharacter:Bool = false)
-	{
-		if (isCharacter)
-		{
-			return FlxAtlasFrames.fromSpriteSheetPacker(loadImage('characters/$key', library), file('images/characters/$key.txt', library));
-		}
-		return FlxAtlasFrames.fromSpriteSheetPacker(loadImage(key, library), file('images/$key.txt', library));
-	}
-    }
+}
