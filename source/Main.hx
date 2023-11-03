5ea315e6520f32f0cbcad74f571cd02953b80016
Blitz <93739827+blitzdotcs@users.noreply.github.com>
2023-11-03 08:14:30 -0400

    FlxStudio/FlixelStudio moment

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,11 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
+		#if desktop
 		addChild(new FlxGame(0, 0, SegaIntro));
+		#else
+		addChild(new FlxGame(0, 0, DebugMenu));
+		#end
+		flixel.addons.studio.FlxStudio.create();
 	}
 }
